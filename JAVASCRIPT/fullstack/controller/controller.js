3de713530d3a5b9fa4f3e8245fb43a6d4f690c5b
William-1206 <irinelciowg@gmail.com>
2024-12-19 21:33:15 -0300

    Add files via upload
    aula 19/12/2024

--- JAVASCRIPT/fullstack/controller/controller.js
+++ JAVASCRIPT/fullstack/controller/controller.js
@@ -4,12 +4,22 @@ const { pessoas } = require('../database/database');
 // CRUD
 // listando usuarios
 function listar() {
-    console.log(pessoas);
+    try {
+        if (pessoas.length <= 0) {
+            console.log("Nenhum usuário cadastrado!")
+        } else {
+            console.log("Usuários:");
+            console.table(pessoas);
+        }
+    } catch (error) {
+        console.error("Erro ao listar os usuários", error.message)
+    }
 }
 
 function cadastrar(id, nome, telefone, email) {
     const novoUsuario = { id, nome, telefone, email }
     pessoas.push(novoUsuario)
+    console.log("Usuário cadastrado com sucesso!")
 }
 
 function editar(id, novoNome, novoTelefone, novoEmail) {
@@ -18,23 +28,43 @@ function editar(id, novoNome, novoTelefone, novoEmail) {
         pessoa.nome = novoNome || pessoa.nome;
         pessoa.telefone = novoTelefone;
         pessoa.email = novoEmail;
+        console.log("Usuário atualizado com sucesso!")
     } else {
         console.log("Usuário não encontrado!")
     }
 }
 
-/*function deletar() {
-    pessoas = []
-}
 
 function deletarPorID(id) {
-    const 
-}*/
+    const usuario = pessoas.findIndex(elemento => elemento.id === id)
+    if (usuario === -1) {
+        console.log("Usuário não encontrado!");
+    } else {
+        pessoas.splice(usuario, 1)
+        console.log("Usuário deletado com sucesso!")
+    }
+
+}
+
+function deletarTodos() {
+    pessoas.length = 0;
+}
+
 
 
 cadastrar(1, "William Gonçalves", "(84)9.9999-9999", "fulano@gmail.com");
 cadastrar(2, "Fulano da Silva", "(84)9.9999-9999", "fulano2@gmail.com");
-editar(1, null, "(84)9.9999-1111", "novoemail@gmail.com");
-//deletarPorID(2)
+editar(1, "William Gonçalves", "(84)9.9999-1111", "novoemail@gmail.com");
+listar();
+deletarPorID(2);
+listar();
+deletarTodos();
+listar();
 
-listar()
+
+
+let teste = 1 // lowercase
+let TESTE1 = 2 // UPPERCASE
+let teste_unitario = 1 // snake case
+let TesteUnitario2 = 1 // CamelCase
+let Pascal = 1 // Pascal case
